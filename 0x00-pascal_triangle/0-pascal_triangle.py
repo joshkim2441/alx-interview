aafa8fc2b84e78e2e765871faf436b803157068b
joshkim2441 <joshkim2441@gmail.com>
2024-06-29 07:57:06 +0300

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,12 +2,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = [[1]]  # Initialize with the first row [1]
+
     for i in range(1, n):
-        row = [1]
+        row = [1]  # First element of each row is 1
         for j in range(1, i):
+            # Calculate the middle elements by summing adjacent elements
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
+        row.append(1)  # Last element of each row is 1
         triangle.append(row)
 
     return triangle
