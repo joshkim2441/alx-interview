7db1f7b7184e6454e3cabffe14f673661dc47f1b
joshkim2441 <joshkim2441@gmail.com>
2024-07-04 19:09:08 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,23 @@
 #!/bin/usr/python3
 
 def canUnlockAll(boxes):
-    opened_boxes = set([0])
-    queue = [0]
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+    Solution to the lockboxes problem
+    """
 
-    while queue:
-        current_box = queue.pop(0)
-        keys = boxes[current_box]
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
 
-        for key in keys:
-            if key not in opened_boxes and key < len(boxes):
-                opened_boxes.add(key)
-                queue.append(key)
-
-    return len(opened_boxes) == len(boxes)
+    for key in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = key in boxes[idx] and key != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
