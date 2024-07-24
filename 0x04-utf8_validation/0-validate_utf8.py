64380a76023b82e67126fcf12bdd933635732f4d
joshkim2441 <joshkim2441@gmail.com>
2024-07-24 23:22:56 +0300

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
 """ Determines if a given data set represents a valid UTF-8 encoding. """
+from itertools import takewhile
+
+
+def int_to_bytes(nums):
+    """ Helper function that converts ints to bits """
+    for num in nums:
+        bytes = []
+        mask = 1 << 8
+        while mask:
+            mask >>= 1
+            bytes.append(bool(num & mask))
+        yield bytes
 
 
 def validUTF8(data):
     """ Initialize a variable to keep track of the
     number of bytes expected.
     """
-    num_bytes = 0
+    num_bytes = int_to_bytes(data)
 
     """ Iterate through each integer in the data """
-    for byte in data:
+    for byte in num_bytes:
         """ Check if the most significant bit (MSB)
          is 0 (single-byte character) """
-        if num_bytes == 0:
-            if byte >> 7 == 0b0:
-                continue
-            elif byte >> 5 == 0b110:
-                num_bytes = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3
-            else:
-                """ Check if the next byte starts with 10
-                (continuation byte). """
-                if byte >> 6 != 0b10:
-                    return False
-                num_bytes -= 1
+        # If single byte char, then valid..
+        if byte[0] == 0:
+            continue
+
+        # At this point, byte is a multi-byte char
+        units = sum(takewhile(bool, byte))
+        if units <= 1:  # UTF-8 can be 1 to 4 bytes long
+            return False
+        if units >= 4:
+            return False
+
+        for _ in range(units - 1):
+            """ Check if the next byte starts with 10
+            (continuation byte). """
+            try:
+                byte = next(num_bytes)
+            except StopIteration:
+                return False
+            if byte[0:2] != [1, 0]:
+                return False
+    return True
 
     return num_bytes == 0
