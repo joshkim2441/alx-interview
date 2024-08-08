404265c430cd44faaaa22909a0aa7f75a4d96e91
joshkim2441 <joshkim2441@gmail.com>
2024-08-08 13:30:45 +0300

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,67 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const movieId = process.argv[2];
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
-  method: 'GET'
+const filmUrl = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
+
+const requestCharacters = async () => {
+  try {
+    const response = await new Promise((resolve, reject) => {
+      request(filmUrl, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          reject(err);
+        } else {
+          resolve(body);
+        }
+      });
+    });
+    const filmData = JSON.parse(response);
+    people = filmData.characters;
+  } catch (error) {
+    console.error('Error fetching characters:', error);
+  }
 };
 
-request(options, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
-
-function printCharacters(characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+const requestNames = async () => {
+  if (people.length > 0) {
+    try {
+      for (const p of people) {
+        const response = await new Promise((resolve, reject) => {
+          request(p, (err, response, body) => {
+            if (err || response.statusCode !== 200) {
+              reject(err);
+            } else {
+              resolve(body);
+            }
+          });
+        });
+        const characterData = JSON.parse(response);
+        names.push(characterData.name);
+      }
+    } catch (error) {
+      if (error.response && error.response.statusCode === 404) {
+        console.error('Error: Movie not found');
+      } else {
+        console.error('Error fetching data:', error);
       }
     }
-  });
-}
+  }
+};
+
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
+  }
+};
+
+getCharNames();
