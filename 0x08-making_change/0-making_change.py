54cf53397db0ff4e2172824c21827ef054d29523
joshkim2441 <joshkim2441@gmail.com>
2024-08-21 16:32:09 +0300

    changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Fewest number of coins needed to meet a given amount total """
+
+def makeChange(coins, total):
+    """ Returns fewest number of coins needed to meet total """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (total * coin)
+    if total != 0:
+        return -1
+    return change
