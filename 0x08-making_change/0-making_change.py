428b09a404542e0497591abc14180d9f33420d4a
joshkim2441 <joshkim2441@gmail.com>
2024-08-21 17:21:18 +0300

    Corrections

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,29 @@ def makeChange(coins, total):
     """ Returns fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-    coins.sort(reverse=True)
-    change = 0
-    for coin in coins:
-        if total <= 0:
-            break
-        temp = total // coin
-        change += temp
-        total -= (total * coin)
-    if total != 0:
+    if coins == [] or coins is None:
         return -1
-    return change
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
